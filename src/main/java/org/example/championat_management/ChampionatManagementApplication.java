cfae5aace10495bf4ce33b8c9ead6e074d636bfe
Donner <152691192+DOnn3r@users.noreply.github.com>
2025-05-01 20:10:20 +0300

    first commit

---
+++ src/main/java/org/example/championat_management/ChampionatManagementApplication.java
@@ -0,0 +1,13 @@
+package org.example.championat_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChampionatManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChampionatManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/championat_management/ChampionatManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.championat_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChampionatManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
