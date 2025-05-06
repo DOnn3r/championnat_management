3d85ad8894e58cf4a760cc5580e879bb01bcc1c7
MitsoaAllan <hei.mitsoa@gmail.com>
2025-05-06 17:52:40 +0300

    chore: Adding saveAll method in ClubCRUDOperation

--- src/main/java/com/fifa_api/dao/operations/ClubCRUDOperation.java
+++ src/main/java/com/fifa_api/dao/operations/ClubCRUDOperation.java
@@ -3,6 +3,8 @@ package com.fifa_api.dao.operations;
 import com.fifa_api.dao.DbConnection;
 import com.fifa_api.dao.mappers.ClubMapper;
 import com.fifa_api.models.Club;
+import com.fifa_api.models.Coach;
+import com.fifa_api.services.exceptions.ServerException;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Repository;
@@ -10,13 +12,16 @@ import org.springframework.stereotype.Repository;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
 public class ClubCRUDOperation implements CRUD<Club> {
     private final DbConnection datasource;
     private final ClubMapper clubMapper;
+    private final CoachCRUDOperation coachCRUDOperation;
 
     @SneakyThrows
     @Override
@@ -66,7 +71,42 @@ public class ClubCRUDOperation implements CRUD<Club> {
     }
 
     @Override
-    public List<Club> saveAll(List<Club> entities) {
-        return List.of();
+    public List<Club> saveAll(List<Club> clubs) {
+        List<Coach> coaches = clubs.stream()
+                .map(Club::getCoach)
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+
+        coachCRUDOperation.saveAll(coaches);
+
+        String sql = "insert into club (id_club, nom, acronyme, annee_creation, nom_stade) " +
+                "values (?, ?, ?, ?, ?) " +
+                "on conflict (id_club) DO update set " +
+                "nom = excluded.nom, " +
+                "acronyme = excluded.acronyme, " +
+                "annee_creation = excluded.annee_creation, " +
+                "nom_stade = excluded.nom_stade";
+
+        try (Connection con = datasource.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            for (Club club : clubs) {
+                if (club.getClubId() == null) {
+                    club.setClubId(UUID.randomUUID());
+                }
+
+                ps.setObject(1, club.getClubId(), Types.OTHER);
+                ps.setString(2, club.getClubName());
+                ps.setString(3, club.getClubAcronym());
+                ps.setInt(4, club.getCreationYear());
+                ps.setString(5, club.getStadium());
+                ps.addBatch();
+            }
+
+            ps.executeBatch();
+            return clubs;
+        } catch (SQLException e) {
+            throw new ServerException(e);
+        }
     }
 }
