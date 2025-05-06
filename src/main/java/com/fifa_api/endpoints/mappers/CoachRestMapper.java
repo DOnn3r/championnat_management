b64266455449ae820bdad0d1e7409b0fbb55dac9
MitsoaAllan <hei.mitsoa@gmail.com>
2025-05-06 19:55:10 +0300

    chore: adding confition if coasch is null and toCoachModel method

--- src/main/java/com/fifa_api/endpoints/mappers/CoachRestMapper.java
+++ src/main/java/com/fifa_api/endpoints/mappers/CoachRestMapper.java
@@ -10,9 +10,23 @@ import java.util.function.Function;
 public class CoachRestMapper implements Function<Coach, CoachRest> {
     @Override
     public CoachRest apply(Coach coach) {
+        if (coach == null) {
+            return null;
+        }
         return new CoachRest(
                 coach.getCoachName(),
                 coach.getCoachNationality()
         );
+    }
+
+    public  Coach toCoachModel(CoachRest coachRest) {
+        String coachName = coachRest.getName();
+        String coachNationality = coachRest.getNationality();
+
+        Coach coach = new Coach();
+        coach.setCoachName(coachName);
+        coach.setCoachNationality(coachNationality);
+
+        return coach;
     }
 }
