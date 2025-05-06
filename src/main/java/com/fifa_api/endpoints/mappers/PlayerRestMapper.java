be043fe550f0fe61e1d8ffe1b89df7842da6563f
MitsoaAllan <hei.mitsoa@gmail.com>
2025-05-06 19:46:39 +0300

    chore: Adding condition if player is null and toModel method

--- src/main/java/com/fifa_api/endpoints/mappers/PlayerRestMapper.java
+++ src/main/java/com/fifa_api/endpoints/mappers/PlayerRestMapper.java
@@ -1,9 +1,12 @@
 package com.fifa_api.endpoints.mappers;
 
+import com.fifa_api.endpoints.rest.CreateOrUpdatePlayer;
 import com.fifa_api.endpoints.rest.PlayerRest;
 import com.fifa_api.models.Player;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -11,6 +14,10 @@ public class PlayerRestMapper {
     private final ClubRestMapper clubRestMapper;
 
     public PlayerRest toRest(Player player) {
+        if (player == null) {
+            return null;
+        }
+
         PlayerRest playerRest = new PlayerRest();
 
         playerRest.setId(player.getPlayerId());
@@ -22,5 +29,20 @@ public class PlayerRestMapper {
        playerRest.setClub(clubRestMapper.apply(player.getClub()));
 
         return playerRest;
+    }
+    public Player toModel(CreateOrUpdatePlayer dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        Player player = new Player();
+        player.setPlayerId(dto.getId() != null ? dto.getId() : UUID.randomUUID());
+        player.setPlayerName(dto.getName());
+        player.setPlayerNumber(dto.getNumber());
+        player.setPost(dto.getPosition());
+        player.setPlayerNationality(dto.getNationality());
+        player.setPlayerAge(dto.getAge());
+
+        return player;
     }
 }
